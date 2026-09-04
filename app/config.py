5925d9dfb0f37d2135c1beed140720f13daefbf9
Edwar Gonzalez <yercken30@gmail.com>
2026-09-03 19:17:04 -0500

    chore: release v0.2.0

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 import sys
 from pathlib import Path
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 APP_NAME = "ToMarkdown"
 APP_ID = "com.yercken.tomarkdown"
